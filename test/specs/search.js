ca387316f9863c8a7380b0d4cb72725184e86230
Viktoryia Kuzmitskaya <Viktoryia_Kuzmitskaya@epam.com>
2021-11-24 00:29:22 +0300

    Added test for empty fields errors while search

---
+++ test/specs/search.js
@@ -0,0 +1,22 @@
+const { expect } = require("chai");
+
+describe("Search", () => {
+  it('should show error messages if required fields are empty', async () => {
+    await browser.url("https://www.jetblue.com/");
+    await browser.maximizeWindow();
+    const acceptCookiesFrame = await $("//iframe[@class='truste_popframe']");
+    await acceptCookiesFrame.waitForDisplayed(3000);
+    await browser.switchToFrame(acceptCookiesFrame);
+    const acceptCookiesButton = await $(".call");
+    await acceptCookiesButton.click();
+    const searchButton = await $('.jb-booker-air-submit');
+    await searchButton.click();  
+    const errorMessages = await $('form').$$('.jb-error');
+    const errorForEmptyDestinationCity = await errorMessages[0].$('span').getText();
+    const errorForEmptyDepartureDate = await errorMessages[1].$('span').getText();
+    const errorForEmptyReturnDate = await errorMessages[2].$('span').getText();
+    expect(errorForEmptyDestinationCity).to.be.equal("Please enter a valid city.");
+    expect(errorForEmptyDepartureDate).to.be.equal("Please enter a valid date.");
+    expect(errorForEmptyReturnDate).to.be.equal("Please enter a valid date.");
+  });
+});
